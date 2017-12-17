169e6d43334fa0b86f9734b67046ff4a96000bde
Damien Daco <dacodamien@gmail.com>
2017-12-17 11:32:38 +0100

    Added a counter to worker

--- app/logic.py
+++ app/logic.py
@@ -5,16 +5,15 @@ class Logic:
     def __init__(self):
         super().__init__()
         print("Logic has been initialized")
-        self.threads = []
+        self.threads = []                   # List for storing multiple threads
 
     def start_thread(self):
 
         self.thread = MultiThreading()
         self.thread.create_thread()
-        self.threads.append(self.thread)
+        self.threads.append(self.thread)    # Store the thread in a list for further use later
 
     def stop_thread(self):
 
-        for thread in self.threads:
-            thread.stop_thread()
-            
+        for thread in self.threads:         # Let's go through the list of threads
+            thread.stop_thread()            # And send the stop signal to each thread
--- app/multithreading.py
+++ app/multithreading.py
@@ -12,9 +12,10 @@ class Worker(QObject):
         print("Hi! I'm your friendly worker and I'm being initialized.")
 
     def task(self):
-
+        i = 0
         while self.is_running:
-            print("I'm your worker and I'm running your task")
+            i += 1
+            print("I'm your worker and I'm running your task number %d" % i)
             time.sleep(1)
 
     def stop(self):
@@ -26,13 +27,11 @@ class MultiThreading(QThread):
 
     def __init__(self):
         super().__init__()
-        #self.__id = None
 
     def create_thread(self):
 
         self.__thread = QThread()
         self.__worker = Worker()
-        #self.__threads.append((self.__thread, self.__worker))
         self.__worker.moveToThread(self.__thread)
         self.__thread.started.connect(self.__worker.task)
         self.__thread.start()
